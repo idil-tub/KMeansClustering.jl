15034911cf4cd0203de9b046d4c785fd54609e53
idil <idilbilgecan@gmail.com>
2024-06-16 22:36:03 +0200

    initial unit tests for new api

--- test/unittests.jl
+++ test/unittests.jl
@@ -15,11 +15,12 @@ end
 @testset "UniformRandomInit" begin
     samples = generate_random_points(100, 2)
     init = UniformRandomInit{Vector{Float64}}()
-    centroids = init(samples, 3)
+    centroids = init(samples, 3)   
     
     @test length(centroids) == 3
     @test all(x -> length(x) == 2, centroids)
 end
+
 
 # Test the buildClusters function
 @testset "buildClusters" begin
@@ -30,19 +31,28 @@ end
     
     @test length(clusters) == 3
     @test all(x -> eltype(x) == Vector{Float64}, clusters)
+    
+    # Verify that each sample is assigned to the nearest centroid
+    for i in 1:length(samples)
+        sample = samples[i]
+        distances = [norm(sample - centroid) for centroid in initial_centroids]
+        min_index = argmin(distances)
+        @test sample in clusters[min_index]
+    end
 end
+
 
 # Test the calculateCenter function
 @testset "calculateCenter" begin
     samples = [rand(2) for _ in 1:10]
-    center = calculateCenter(samples)  
+    center = calculateCenter(samples) 
+
     @test length(center) == 2
     @test typeof(center) == Vector{Float64}
 
-    samples2 = [rand(3) for _ in 1:10]    
-    center2 = calculateCenter(samples2)   
-    @test length(center2) == 3
-    @test typeof(center2) == Vector{Float64}
+    # Verify center is the mean of samples
+    calculated_mean = mean(samples)
+    @test isapprox(center, calculated_mean, atol=1e-6)
 end
 
 # Test the KMeans function
@@ -52,8 +62,10 @@ end
     result = KMeans(samples, k)
     
     @test length(result) == k
+
     for (center, cluster) in result
         @test typeof(center) == Vector{Float64}
         @test eltype(cluster) == Vector{Float64}
     end
 end
+
